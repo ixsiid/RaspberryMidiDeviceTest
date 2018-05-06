9fdd27698a1ccc2c5bf95fd6eb06af4ed1b4a757
ixsiid <ixsiid@halzion.net>
2018-05-06 22:23:12 +0000

    play midi file

--- mkt.cpp
+++ mkt.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <chrono>
 #include <thread>
+#include <unistd.h>
 #include "RtMidi.h"
 #include "SmfParser.hpp"
 
@@ -20,7 +21,7 @@ void mycallback(double deltatime, std::vector<unsigned char> *message, void *use
 	if ( message->size() > 0 ) {
 //		std::cout << "stamp = " << deltatime << std::endl;
 //		printf("type = %d\n", (*message)[0]);
-		(*message)[1] = 64;
+//		(*message)[1] = 64;
 		((RtMidiOut *)userData)->sendMessage(message);
 	}
 }
@@ -59,10 +60,10 @@ void listMidiPort (std::string inPortName, int * inPortIndex, std::string outPor
 	}
 }
 
-struct Note {
+typedef struct _Note {
 	int time;
 	unsigned char message[3];
-};
+} Note;
 
 bool compare_event(const ch_event & left, const ch_event & right) {
 	if (left.acumulate_time == right.acumulate_time) {
@@ -84,6 +85,12 @@ std::ostream & operator<<(std::ostream & Str, const Note & note) {
 	}
 	return Str;
 }
+
+typedef struct _Smf {
+	std::string filepath;
+	Note * notes;
+	int count;
+} Smf;
 
 
 int main()
@@ -113,19 +120,16 @@ int main()
 	midiin->setCallback(&mycallback, midiout);
 
 	//
-	std::vector<std::string> files = {
-		"magic_music.mid"
+	std::vector<Smf> files = {
+		{"magic_music.mid", NULL, -1}
 	};
 
-	Note * playNotes = NULL;
-	int playNotesCount = 0;
-	
-	for (auto x : files) {
-		SmfParser smf = SmfParser(x.c_str());
+	for (int i=0; i<1; i++) {
+		Smf * x = &(files[i]);
+		std::cout << "parse: " << x->filepath << std::endl;
+		SmfParser smf = SmfParser(x->filepath.c_str());
 		smf.printcredits();
-		std::cout << "parse" << std::endl;
 		smf.parse(0);
-		std::cout << "abstract" << std::endl;
 		smf.abstract();
 		std::cout << std::endl;
 		std::cout << "ticks per beat: " << smf.getTicks_per_beat() << std::endl;
@@ -136,58 +140,73 @@ int main()
 		}
 
 		std::sort(smf.events.begin(), smf.events.end(), compare_event);
-		Note notes[noteCount];
-		int i = 0;
+		x->notes = new Note[noteCount];
+		std::cout << "notes: " << x->notes << std::endl;
+		x->count = 0;
 		int tempo = smf.getBpm();
 		int tickPerBeat = smf.getTicks_per_beat();
 		float k = 60000.0f / (tempo * tickPerBeat);
 		std::cout << "tempo " << tempo << ", tpb " << tickPerBeat << ", k=" << k << std::endl;
 		for (auto y : smf.events) {
 			if (y.event_type == 9) {
-				notes[i].time = (int)(y.acumulate_time * k);
-				notes[i].message[0] = 144;
-				notes[i].message[1] = y.par1;
-				notes[i].message[2] = y.par2;
-				i++;
+				x->notes[x->count].time = (int)(y.acumulate_time * k);
+				x->notes[x->count].message[0] = 144;
+				x->notes[x->count].message[1] = y.par1;
+				x->notes[x->count].message[2] = y.par2;
+				x->count++;
 			} else if (y.event_type == 8) {
-				notes[i].time = (int)(y.acumulate_time * k);
-				notes[i].message[0] = 128;
-				notes[i].message[1] = y.par1;
-				notes[i].message[2] = 0;
-				i++;
+				x->notes[x->count].time = (int)(y.acumulate_time * k);
+				x->notes[x->count].message[0] = 128;
+				x->notes[x->count].message[1] = y.par1;
+				x->notes[x->count].message[2] = 0;
+				x->count++;
 			}
 		}
 
-		playNotes = &(notes[0]);
-		playNotesCount = noteCount;
+		std::cout << "count: " << x->count << std::endl;
 	}
+
+	Smf * play = &(files[0]);
 
 	// Don't ignore sysex, timing, or active sensing messages.
 	midiin->ignoreTypes( false, false, false );
 	std::cout << "\nReading MIDI input ... press <enter> to quit.\n";
 
-	std::thread thr([](Note * notes, int count, RtMidiOut * midi) {
-		std::cout << "Play notes count: " << count << std::endl;
+	std::thread thr([](Smf * smf, RtMidiOut * midi) {
+		std::cout << "notes addr: " << smf->notes << std::endl;
+		std::cout << "Play notes count: " << smf->count << std::endl;
 		auto start = std::chrono::system_clock::now();
 		int i = -1;
+		long j = 0;
+		Note * note = smf->notes;
 		while(true) {
 			int acumulate = std::chrono::duration_cast<std::chrono::milliseconds>(
 				std::chrono::system_clock::now() - start
 			).count();
+
+			if (j++ % 100000 == 0) std::cout << "\racumlatetime: " << acumulate << "[msec] (" << j << ")";
+			fflush(stdout);
 			while(true) {
-				if (i++ >= count) return;
-				Note note = notes[i];
-				if (note.time <= acumulate) {
-					midi->sendMessage(note.message, 3);
-					std::cout << "time=" << acumulate << ", " <<  note << std::endl;
+				if (i >= smf->count) {
+					std::cout << "play end" << std::endl;
+					return;
+				}
+
+				if (note->time <= acumulate) {
+					i++;
+					midi->sendMessage(note->message, 3);
+					std::cout << "\rtime=" << acumulate << ", " <<  *note << std::endl;
+					note++;
+//					std::cout << "next: " << *note << std::endl;
 				} else {
 					break;
 				}
+				usleep(1666);
 			}
 		}
-		std::cout << "play end" << std::endl;
+		std::cout << "unknown finish to play" << std::endl;
 		return;
-	}, playNotes, playNotesCount, midiout);
+	}, play, midiout);
 	thr.detach();
 
 	char input;
